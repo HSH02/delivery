f595c2b6d9e1fe111653e245c3d5d43935cb3c73
HSH02 <tkrhktkdwk10@gmail.com>
2025-04-08 23:07:36 +0900

    chore : 멀티 모듈 설정

--- api/src/main/java/org/delivery/api/ApiApplication.java
+++ api/src/main/java/org/delivery/api/ApiApplication.java
@@ -1,13 +1,12 @@
-package org.delivery.service;
+package org.delivery.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeliveryApplication {
+public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DeliveryApplication.class, args);
+		SpringApplication.run(ApiApplication.class, args);
 	}
-
 }
